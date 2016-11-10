8ebe3416cb296509d9b2c0ac08fc238076beac97
jinglou <jinglou@gmail.com>
2016-11-10 23:14:32 +0800

    RSS
    RSS

--- RSS/RSS.m
+++ RSS/RSS.m
@@ -2,6 +2,7 @@
 % 
 % Jing Lou, Wei Zhu, Huan Wang, Mingwu Ren, "Small Target Detection Combining Regional Stability and Saliency in a Color Image," 
 % Multimedia Tools and Applications, pp. 1-18, 2016. doi:10.1007/s11042-016-4025-7
+%
 % Project page: http://www.loujing.com/rss-small-target/
 % 
 % Copyright (C) 2016 Jing Lou
