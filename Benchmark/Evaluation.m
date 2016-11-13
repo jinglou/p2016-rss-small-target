0efb2ed4ac33bc7f317e2632ed63b5e802e1659a
jinglou <jinglou@gmail.com>
2016-11-13 22:46:11 +0800

    Benchmark
    Benchmark

--- Benchmark/Evaluation.m
+++ Benchmark/Evaluation.m
@@ -5,6 +5,10 @@
 % Multimedia Tools and Applications, pp. 1-18, 2016. doi:10.1007/s11042-016-4025-7
 %
 % Project page: http://www.loujing.com/rss-small-target/
+% 
+% Copyright (C) 2016 Jing Lou
+%
+% The usage of this code is restricted for non-profit research usage only and using of the code is at the user's risk.
 % ------------------------------------------------------------------------------------------------------
 %
 % Notes:
@@ -21,11 +25,7 @@
 %		   for each image.
 % 		b) In the root directory, the resulting file "Stat.mat" includes the average values of Precision, Recall and F-measure 
 %		   for each model. 
-%
 % 
-% Copyright (C) 2016 Jing Lou
-%
-% The usage of this code is restricted for non-profit research usage only and using of the code is at the user's risk.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clc; clear; close all;
